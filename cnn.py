dc40765e0cbf3a1cac43f96fae67d4c83380c280
sarvasvkulpati <sarvasvkulpati@gmail.com>
2018-01-19 19:35:00 +0800

    added comments

--- cnn.py
+++ cnn.py
@@ -10,7 +10,7 @@ from keras.datasets import mnist
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
  
-
+#initialize and normalize data
 X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
 X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
 X_train = X_train.astype('float32')
@@ -23,11 +23,13 @@ Y_test = np_utils.to_categorical(y_test, 10)
 
 model = Sequential()
  
+#add first conv layer
 model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(28,28,1)))
 model.add(Convolution2D(32, 3, 3, activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
  
+#this goes from the 2d conv layers to a binary classification
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
